88347b1bbddfabc5839c70c85df84e6db2d166dc
= <lyulucca@gmail.com>
2023-01-13 15:09:05 -0300

    fix: buttons now change color when theme is changed

--- lampadaJS/lampada.js
+++ lampadaJS/lampada.js
@@ -1,4 +1,5 @@
 const titulo = document.getElementById('heading')
+//const botao = document.getElementsByClassName('btn')
 const turnOn = document.getElementById('turn-on');
 const turnOff = document.getElementById('turn-off');
 const lamp = document.getElementById('lamp');
@@ -12,6 +13,10 @@ function lampOn (){
         lamp.src = '/img/ligada.jpg';
         body.style.backgroundColor = '#ffff';
         titulo.style.color = '#000';
+        turnOn.style.backgroundColor = '#000';
+        turnOff.style.backgroundColor = '#000';
+        turnOn.style.color = '#ffff';
+        turnOff.style.color = '#ffff';
     }
 }
 
@@ -21,6 +26,10 @@ function lampOff (){
         lamp.src = '/img/desligado.png';
         body.style.backgroundColor = 'rgb(41, 41, 41)';
         titulo.style.color = '#ffff'
+        turnOn.style.backgroundColor = '#ffff';
+        turnOff.style.backgroundColor = '#ffff';
+        turnOn.style.color = '#000';
+        turnOff.style.color = '#000';
     }
 }
 
@@ -29,6 +38,10 @@ function lampBreaking (){
         lamp.src = '/img/quebrada.png';
         audioB.play();
         titulo.style.color = '#ffff'
+        turnOn.style.backgroundColor = '#ffff';
+        turnOff.style.backgroundColor = '#ffff';
+        turnOn.style.color = '#000';
+        turnOff.style.color = '#000';
     }
     body.style.backgroundColor = 'rgb(41, 41, 41)';
 }
